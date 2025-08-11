6656b6a7e9819ef115ee1b8ed3eed3e266fbe043
Ning Li <2212363918@qq.com>
2025-08-11 07:18:16 +0000

    Update operator prompt

--- mobile_use/default_prompts/prompt_type.py
+++ mobile_use/default_prompts/prompt_type.py
@@ -4,10 +4,12 @@ from pathlib import Path
 
 import yaml
 
-def load_prompt(prompt_type = Literal["planner"], prompt_config: str=None) -> Optional["Prompt"]:
+def load_prompt(prompt_type = Literal["planner", "operator"], prompt_config: str=None) -> Optional["Prompt"]:
     match prompt_type:
         case "planner":
             return PlannerPrompt(config=prompt_config) if prompt_config else PlannerPrompt()
+        case "operator":
+            return OperatorPrompt(config=prompt_config) if prompt_config else OperatorPrompt()
         case _:
             raise KeyError(f"Unknown prompt type: {prompt_type}")
 
@@ -34,10 +36,31 @@ class PlannerPrompt(Prompt):
     continue_plan: str = ""
 
 
+@dataclass
+class OperatorPrompt(Prompt):
+    config: str = "operator.yaml"
+    system_prompt: str = ""
+    init_tips: str = ""
+    task_prompt: str = ""
+    device_time_prompt: str = ""
+    plan_prompt: str = ""
+    subgoal_prompt: str = ""
+    history_prompt: str = ""
+    progress_prompt: str = ""
+    memory_prompt: str = ""
+    reflection_prompt: str = ""
+    long_reflection_prompt: str = ""
+    global_reflection_prompt: str = ""
+    observation_prompt: str = ""
+    response_prompt: str = ""
+
+
 if __name__ == "__main__":
-    prompt = load_prompt("planner")
+    # prompt = load_prompt("planner")
+    # print(prompt)
+    # print(prompt.system_prompt)
+    # print(prompt.task_prompt)
+    # print(prompt.init_plan)
+    # print(prompt.continue_plan)
+    prompt = load_prompt("operator")
     print(prompt)
-    print(prompt.system_prompt)
-    print(prompt.task_prompt)
-    print(prompt.init_plan)
-    print(prompt.continue_plan)
