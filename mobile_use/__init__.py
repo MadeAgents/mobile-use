ecd6224140571a21249a277314362c887c4d746e
lokinko <lokinko@oppo.com>
2025-08-11 11:05:56 +0000

    style: move utils into common utils.

--- mobile_use/__init__.py
+++
@@ -1,4 +0,0 @@
-from mobile_use.environ import Environment
-from mobile_use.vlm import VLMWrapper
-from mobile_use.agents import *
-from .scheme import *
---
+++ mobile_use/utils/__init__.py
@@ -0,0 +1,5 @@
+from mobile_use.utils.vlm import VLMWrapper
+from mobile_use.utils.logger import setup_logger
+
+from mobile_use.utils.utils import encode_image_url, contains_chinese, smart_resize, remove_img_placeholder, \
+    compare_image, is_same_image, diff_image
