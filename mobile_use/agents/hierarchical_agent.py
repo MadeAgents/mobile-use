a171604fec9091bffab14d588db33fc37199902f
Ning Li <2212363918@qq.com>
2025-09-05 06:38:14 +0000

    small fix

--- mobile_use/agents/hierarchical_agent.py
+++ mobile_use/agents/hierarchical_agent.py
@@ -361,7 +361,7 @@ class HierarchicalAgent(Agent):
                         logger.warning(f"Failed to parse the trajectory reflection. Error: {e}")
 
         # Call AnswerAgent
-        if self.status == AgentStatus.FINISHED and answer is None:
+        if self.status == AgentStatus.FINISHED and answer is None and self.answer_agent:
             answer_messages = self.answer_agent.get_message(self.episode_data)
             show_message(answer_messages, "Answer")
             response = self.answer_agent.vlm.predict(answer_messages)
